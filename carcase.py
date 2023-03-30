7aa32f703fff45b392b6fd9b696de1e3ea460a29
RuilinWang <jasonwangruilin@gmail.com>
2023-03-30 13:55:18 +0100

    Add tutorial

--- carcase.py
+++ carcase.py
@@ -5,11 +5,20 @@ import ADSTP.IntegratedPipeline as ip
 integrated_pipeline = ip.general_datastory_pipeline
 # Set replace variables names
 readable_names = dict((kv.split(': ') for kv in (l.strip(' \n') for l in open('./data/readableNamesForTenData.txt'))))
-# Read dataset
-Car_dataset = pandas.read_csv('./data/car data.csv')
+# # Read dataset
+# Car_dataset = pandas.read_csv('./data/car data.csv')
+# # Select target columns
+# Xcol = ['Present_Price', 'Kms_Driven', 'Year']; ycol = 'Selling_Price'
+# # Data preprocessing.
+# cleandata=d2t.remove_outliers(Car_dataset,Xcol,ycol)
+# # Fit the data to the prototype.
+# integrated_pipeline.LinearFit(cleandata, Xcol, ycol, [readable_names.get(key) for key in Xcol], readable_names.get(ycol))
+
+# # Read dataset
+fish_dataset=pandas.read_csv('./data/fish.csv')
 # Select target columns
-Xcol = ['Present_Price', 'Kms_Driven', 'Year']; ycol = 'Selling_Price'
-# Data preprocessing.
-cleandata=d2t.remove_outliers(Car_dataset,Xcol,ycol)
+Xcol = ['Length', 'Diagonal', 'Height', 'Width']; ycol = 'Weight'
+# # Data preprocessing.
+# cleandata=d2t.remove_outliers(fish_dataset,Xcol,ycol)
 # Fit the data to the prototype.
-integrated_pipeline.LinearFit(cleandata, Xcol, ycol, [readable_names.get(key) for key in Xcol], readable_names.get(ycol))
+integrated_pipeline.LinearFit(fish_dataset, Xcol, ycol, [readable_names.get(key) for key in Xcol], readable_names.get(ycol))
